03aa1ad47eb8775efd90969579569b6f28af9d4a
lvypas <lyubic@gmail.com>
2013-08-30 16:18:04 +0300

    Tutorial 25,26 - Writing Our First Aspect Part 1.

---
+++ src/org/lvypas/AopMain.java
@@ -0,0 +1,20 @@
+package org.lvypas;
+
+import org.lvypas.service.ShapeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AopMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+        System.out.println(shapeService.getCircle().getName());
+
+    }
+
+}
