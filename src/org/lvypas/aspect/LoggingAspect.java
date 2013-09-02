e508181decec542a8b05e810a4d19e1ff42a75b6
lvypas <lyubic@gmail.com>
2013-09-02 12:07:54 +0300

    Tutorial 28 - Pointcuts and Wildcard Expressions.

--- src/org/lvypas/aspect/LoggingAspect.java
+++ src/org/lvypas/aspect/LoggingAspect.java
@@ -2,12 +2,23 @@ package org.lvypas.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public String getName())")
+    @Before("allGetters()")
     public void LoggingAdvice() {
         System.out.println("Advice run. Get method called.");
+    }
+    
+    @Before("allGetters()")
+    public void secondAdvice() {
+        System.out.println("Second Advice executed.");
+    }
+    
+    @Pointcut("execution(* get*())")
+    public void allGetters() {
+        
     }
 }
